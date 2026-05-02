d83cdca8d81a3bb01b9fced1d5e9427668e86388
nafeesa zaka <nafeesazaka536@gmail.com>
2026-05-02 13:44:51 +0500

    Fix Express 5 wildcard route syntax

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.use('/api/payroll', require('./routes/payrollRoutes'));
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/dist')));
 
-  app.get('*', (req, res) =>
+  app.get('/*splat', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
   );
 } else {
